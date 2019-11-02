3f684343a354ba44314ddadcd7e16d4b6245c9db
Y-Kawamura-Work <57284258+Y-Kawamura-Work@users.noreply.github.com>
2019-11-02 18:24:28 +0900

    Add files via upload

---
+++ jquery.js
@@ -0,0 +1,35 @@
+$(function() {
+	$('.close-modal').click(function() {
+		$('#modal-wrapper-one').fadeOut();
+	});
+	
+	$('.one').click(function() {
+		$('#modal-wrapper-one').fadeIn();
+	});
+	
+	$('.close-modal').click(function() {
+		$('#modal-wrapper-two').fadeOut();
+	});
+	
+	$('.two').click(function() {
+		$('#modal-wrapper-two').fadeIn();
+	});
+	
+	$('.close-modal').click(function() {
+		$('#modal-wrapper-three').fadeOut();
+	});
+	
+	$('.three').click(function() {
+		$('#modal-wrapper-three').fadeIn();
+	});
+	
+	$('.close-modal').click(function() {
+		$('#modal-wrapper-four').fadeOut();
+	});
+	
+	$('.four').click(function() {
+		$('#modal-wrapper-four').fadeIn();
+	});
+
+
+});
